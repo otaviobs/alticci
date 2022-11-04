7cb82d61afecf94eb09c41b85b67d7b4a63687fd
Otávio Pessoal <otavio.basiliosaraiva@gmail.com>
2022-11-04 12:11:07 -0300

    clean project

--- src/main/java/com/alticci/controller/SequenceController.java
+++ src/main/java/com/alticci/controller/SequenceController.java
@@ -12,9 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
-@Tag(name = "Alticci", description = "API calculates Alticci's sequence.")
+@Tag(name = "Alticci", description = "" +
+        "API calculates Alticci's sequence." +
+        "Uses a range of numbers because I am working with Ingeter type then is a limit number. So you can input number between 0 and 78.")
 @RestController
 @RequestMapping("/alticci")
 public class SequenceController {
@@ -24,7 +28,7 @@ public class SequenceController {
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Return Alticci sequence number as you input index number."),
-            @ApiResponse(responseCode = "400", description = "If you send a negative number will return a warning message API can't process it."),
+            @ApiResponse(responseCode = "400", description = "Case input number upper than 78 or lower than zero show the message with BAD REQUEST code."),
     })
     @GetMapping("/{number}")
     @Cacheable(value = "numberCalculated")
@@ -34,9 +38,9 @@ public class SequenceController {
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Return Alticci sequence number as you input index number."),
-            @ApiResponse(responseCode = "400", description = "If you send a negative number will return a warning message API can't process it."),
+            @ApiResponse(responseCode = "400", description = "Case input number upper than 78 or lower than zero show the message with BAD REQUEST code."),
     })
-    @GetMapping("/{beginNumber}/{endNumber}")
+    @GetMapping("/between/{beginNumber}/{endNumber}")
     @Cacheable(value = "numberCalculated")
     public List<Integer> getListSequence(@PathVariable Integer beginNumber, @PathVariable Integer endNumber) {
         return sequenceService.getListDescSequence(beginNumber, endNumber);
