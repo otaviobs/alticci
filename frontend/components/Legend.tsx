c8d07dd54b2811b350cda7c3da7f76511618ca95
Otávio Pessoal <otavio.basiliosaraiva@gmail.com>
2022-11-08 10:19:53 -0300

    add message

--- frontend/components/Legend.tsx
+++ frontend/components/Legend.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Legend = () => {
   return (
-    <div className='h-14 my-4 py-3 border text-center'>
-        Input a number between 0 and 78 because the API is working with Integer type that has a limit number.
+    <div className='h-14 my-4 border text-center'>
+        <p>
+          Input a number between 0 and 78 because the API is working with Integer type that has a limit number.
+          <br />
+          <small>*It has a low range of numbers because the task description show lows numbers</small>
+        </p>
     </div>
   )
 }
